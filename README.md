365a766152fe7cb1fa9c61246c3e77d9d7a16c08
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-03-16 15:44:10 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rimatomics Nukes Go Brrr ![image](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Fshieldsio-steam-workshop.jross.me%2F3445411908)
+
+Travel speed upgrade for the Rimatomics Liberator missile.
+
+Find it [here](https://steamcommunity.com/sharedfiles/filedetails/?id=3445411908) on the Steam workshop.
---
+++ Source/NukesGoBrrr/ThisDefOf.cs
@@ -0,0 +1,16 @@
+﻿using Rimatomics;
+using RimWorld;
+
+namespace NukesGoBrrr
+{
+    [DefOf]
+    public static class ThisDefOf
+    {
+        public static ResearchStepDef RimatomicsNukesGoBrrr_SPM3;
+
+        static ThisDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(ThisDefOf));
+        }
+    }
+}
