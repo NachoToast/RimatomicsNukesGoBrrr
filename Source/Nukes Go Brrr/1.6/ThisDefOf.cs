f2f785f06f38dd9d25ad69e43f506384c4020ecf
NachoToast <32235595+NachoToast@users.noreply.github.com>
2025-06-14 14:40:51 +1200

    v1.6

---
+++ Source/Nukes Go Brrr/1.6/ThisDefOf.cs
@@ -0,0 +1,15 @@
+﻿using Rimatomics;
+using RimWorld;
+
+namespace NukesGoBrrr;
+
+[DefOf]
+public static class ThisDefOf
+{
+    public static ResearchStepDef RimatomicsNukesGoBrrr_SPM3;
+
+    static ThisDefOf()
+    {
+        DefOfHelper.EnsureInitializedInCtor(typeof(ThisDefOf));
+    }
+}
